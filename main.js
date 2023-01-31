2ac10c0fe3702ba214b3910190392c910367d4fb
shen <santi.dionis@gmail.com>
2023-01-31 20:12:09 +0100

    ADD: JavaScript

---
+++ main.js
@@ -0,0 +1,9 @@
+let bill = document.querySelector('.inputs-section__bill-input');
+let people = document.querySelector('.inputs-section__people-input');
+let buttons = document.querySelectorAll('.btns__button');
+
+buttons.forEach(element => {
+   console.log(element) 
+})
+
+
