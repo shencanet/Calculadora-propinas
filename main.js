c39d746b49e26c53c9827f37dc6403b87d831c1c
shen <santi.dionis@gmail.com>
2023-01-31 21:58:11 +0100

    ADD: JavaScript

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let billNumber = parseInt(bill.value)
 let people = document.querySelector('.inputs-section__people-input');
 let peopleNumber = parseInt(people.value)
 let tipResult = document.querySelector('.results__tip-value');
+let totalResult = document.querySelector('.results__total-value')
 let buttons = document.querySelectorAll('.btns__button');
 
 buttons.forEach(element => {
@@ -11,7 +12,10 @@ buttons.forEach(element => {
     //console.log(tipValue);
     //calculo de la propina
   
-   tipResult.innerText = (((billNumber * tipValue) / 100)/peopleNumber)
+   tipResult.innerText = (((billNumber * tipValue) / 100)/peopleNumber);
+   //calculo del total
+   totalResult.innerHTML = (((billNumber * tipValue / 100 ) + billNumber)/peopleNumber);
+
     
   });
 })
