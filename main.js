7a59c230714874708b9fc481b1137cb267f009b2
shen <santi.dionis@gmail.com>
2023-02-02 19:23:38 +0100

    ADD: JavaScript

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
-let bill = document.querySelector('.inputs-section__bill-input');
-let billNumber = parseInt(bill.value)
-let people = document.querySelector('.inputs-section__people-input');
-let peopleNumber = parseInt(people.value)
-let tipResult = document.querySelector('.results__tip-value');
-let totalResult = document.querySelector('.results__total-value')
-let buttons = document.querySelectorAll('.btns__button');
+let bill = document.querySelector(".inputs-section__bill-input");
+let billNumber = parseInt(bill.value);
+let people = document.querySelector(".inputs-section__people-input");
+let peopleNumber = parseInt(people.value);
+let tipResult = document.querySelector(".results__tip-value");
+let totalResult = document.querySelector(".results__total-value");
+let buttons = document.querySelectorAll(".btns__button");
 
 buttons.forEach(element => {
-  element.addEventListener('click', event=>{
-    //CAMBIAR ESTILOS
-    buttons.forEach(element =>{
-      element.classList.remove('btns__button--selected');
-    } ) 
-    element.classList.add('btns__button--selected');
+  element.addEventListener("click", event => {
+ 
+    buttons.forEach(element => {
+      element.classList.remove('btns__button--selected')
+      });
 
+      element.classList.add('btns__button--selected');
+    
 
+    let tipValue = parseInt(event.target.innerText.slice(0, -1));
+   
 
-    let tipValue = parseInt(event.target.innerText.slice(0, -1))
-    //console.log(tipValue);
-    //calculo de la propina
-  
-   tipResult.innerText = (((billNumber * tipValue) / 100) / peopleNumber);
-   //calculo del total
-   totalResult.innerText = (((billNumber * tipValue) / 100  + billNumber)/peopleNumber);
-
-
+    tipResult.innerText = ((billNumber * tipValue / 100) / peopleNumber);
+    
+    totalResult.innerText = (((billNumber * tipValue / 100) + billNumber) / peopleNumber);
     
   });
 })
-
 
